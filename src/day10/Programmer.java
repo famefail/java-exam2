020cc5b9767bf4d5ba96c177f09700da62ec2976
famefail <the_boy_fail@hotmail.com>
2021-10-20 14:16:21 +0700

    exam2

---
+++ src/day10/Programmer.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package day10;
+
+/**
+ *
+ * @author Boy
+ */
+public class Programmer extends Employee {
+    public Programmer(String FirstnameInput ,int SalaryInput){
+        super(FirstnameInput , SalaryInput);
+    }
+    public void createWebsite(String template ,String title){
+        
+    }
+    public void installWindows(String version ,String productKey){
+        
+    }
+    
+}
